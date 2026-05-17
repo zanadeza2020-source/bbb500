a7d7eefa2da8dde7569d11038471f806478476ab
NADER 202 <zanadeza2020@gmail.com>
2026-05-17 08:19:20 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = "medterm-cache-v4";
+
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installing...");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  // تخزين ملفات قاعدة البيانات
+  if (request.url.includes("/database/")) {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, response.clone());
+            return response;
+          });
+        })
+        .catch(() => caches.match(request))
+    );
+    return;
+  }
+
+  // استراتيجية cache first
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
